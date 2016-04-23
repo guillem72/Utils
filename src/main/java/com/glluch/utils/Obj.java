fa09a60e6ba683c7abc756f34ec4cb2389deeedb
Guillem Lluch Moll <guillem72@gmail.com>
2016-04-23 10:54:32 +0200

    A class for simpler output and for knowing the class of an object added.

---
+++ src/main/java/com/glluch/utils/Obj.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.glluch.utils;
+
+/**
+ *
+ * @author Guillem LLuch Moll guillem72@gmail.com
+ */
+public class Obj {
+ public static String typeOf(Object obj) {
+    Class cls = obj.getClass();
+    return cls.getName();
+}   
+}
--- src/main/java/com/glluch/utils/Out.java
+++ src/main/java/com/glluch/utils/Out.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.glluch.utils;
 
 import java.util.List;
@@ -56,5 +51,13 @@ public class Out {
         }
         System.out.println(res);
         return res;
+    }
+    
+    public static void p(Object o){
+        System.out.println(o.toString());
+    }
+    
+    public static void typeOf(Object o){
+        p(Obj.typeOf(o));
     }
 }
